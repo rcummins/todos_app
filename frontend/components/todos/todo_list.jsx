6c86bc1b0561749df726ef1d39be3ad40d6aa0b1
Renata Cummins <renatacummins@gmail.com>
2019-09-03 14:18:23 -0400

    feature: display each todo's title in an unordered list

--- frontend/components/todos/todo_list.jsx
+++ frontend/components/todos/todo_list.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import TodoListItem from './todo_list_item';
 
-const TodoList = () => (
-  <h3>Todo list goes here</h3>
+const TodoList = ({ todos }) => (
+  <ul>
+    {
+      todos.map( (todo, index) => (
+        <TodoListItem key={index} todo={todo} />
+      ))
+    }
+  </ul>
 );
 
 export default TodoList;
