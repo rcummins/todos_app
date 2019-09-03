db00d3a8055e0811a69a1ee7e38e7ec0b2463386
Renata Cummins <renatacummins@gmail.com>
2019-09-03 11:21:33 -0400

    feature: give reducer the ability to receive one or multiple todos

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,9 +1,34 @@
 import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todo_actions';
 
-const todosReducer = ( oldState = {}, action ) => {
+const initialState = {
+  1: {
+    id: 1,
+    title: "cook dinner",
+    body: "macaroni and cheese",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "bake cake",
+    body: "vanilla with buttercream frosting",
+    done: true
+  }
+};
+
+const todosReducer = ( oldState = initialState, action ) => {
   Object.freeze(oldState);
 
   switch (action.type) {
+    case RECEIVE_TODOS:
+      let newState = {};
+      for (let i = 0; i < action.todos.length; i++) {
+        newState[action.todos[i].id] = action.todos[i];
+      }
+      return newState;
+    case RECEIVE_TODO:
+      let newTodo = {};
+      newTodo[action.todo.id] = action.todo;
+      return Object.assign({}, oldState, newTodo);
     default:
       return oldState;
   }
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import configureStore from './store/store';
+import { receiveTodos, receiveTodo } from './actions/todo_actions';
 
 const store = configureStore();
 window.store = store;
+window.receiveTodos = receiveTodos;
+window.receiveTodo = receiveTodo;
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
