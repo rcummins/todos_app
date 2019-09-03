e28d1f3052e9bd53dad0df41ddd60ce4d2b22a0b
Renata Cummins <renatacummins@gmail.com>
2019-09-03 09:39:52 -0400

    feature: add store and put it on the window

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+
+const store = configureStore();
+window.store = store;
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <h1>Todos App</h1>,
