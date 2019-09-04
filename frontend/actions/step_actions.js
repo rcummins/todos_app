54875e26aefcdb0d5e65a03c0e22eaff24b6bddb
Renata Cummins <renatacummins@gmail.com>
2019-09-04 14:46:26 -0400

    feature: add step actions and reducer

---
+++ frontend/actions/step_actions.js
@@ -0,0 +1,24 @@
+export const RECEIVE_STEPS = "RECEIVE_STEPS";
+export const RECEIVE_STEP = "RECEIVE_STEP";
+export const REMOVE_STEP = "REMOVE_STEP";
+export const TOGGLE_STEP_STATUS = "TOGGLE_STEP_STATUS";
+
+export const receiveSteps = steps => ({
+  type: RECEIVE_STEPS,
+  steps
+});
+
+export const receiveStep = step => ({
+  type: RECEIVE_STEP,
+  step
+});
+
+export const removeStep = id => ({
+  type: REMOVE_STEP,
+  id
+});
+
+export const toggleStepStatus = id => ({
+  type:TOGGLE_STEP_STATUS,
+  id
+});
---
+++ frontend/reducers/steps_reducer.js
@@ -0,0 +1,59 @@
+import {
+  RECEIVE_STEPS,
+  RECEIVE_STEP,
+  REMOVE_STEP,
+  TOGGLE_STEP_STATUS
+} from '../actions/step_actions';
+
+const initialState = {
+  1: {
+    id: 1,
+    title: 'boil pasta',
+    done: false,
+    todo_id: 1
+  },
+  2: {
+    id: 2,
+    title: 'make cheese sauce',
+    done: false,
+    todo_id: 1
+  }
+};
+
+const stepsReducer = ( oldState = initialState, action ) => {
+  Object.freeze(oldState);
+
+  let newState;
+  let newStep;
+  let idToDelete;
+  switch (action.type) {
+
+    case RECEIVE_STEPS:
+      newState = {};
+      for (let i = 0; i < action.steps.length; i++) {
+        newState[action.steps[i].id] = action.steps[i];
+      }
+      return newState;
+
+    case RECEIVE_STEP:
+      newStep = {};
+      newStep[action.step.id] = action.step;
+      return Object.assign({}, oldState, newStep);
+
+    case REMOVE_STEP:
+      newState = Object.assign({}, oldState);
+      idToDelete = action.id;
+      delete newState[idToDelete];
+      return newState;
+
+    case TOGGLE_STEP_STATUS:
+      newState = Object.assign({}, oldState);
+      newState[action.id].done = !newState[action.id].done;
+      return newState;
+
+    default:
+      return oldState;
+  }
+};
+
+export default stepsReducer;
