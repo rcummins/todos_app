b82f20a6c9711be820c738ce00f08ecf4539d763
Renata Cummins <renatacummins@gmail.com>
2019-09-05 11:38:26 -0400

    feature: add button to delete step

--- frontend/components/steps/step_list_item.jsx
+++ frontend/components/steps/step_list_item.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
-const StepListItem = ({ step, removeStep, toggleStepStatus }) => (
-  <div>
-    <h4>{step.title}</h4>
-    <p>{step.body}</p>
-    <p>Buttons to toggle step status and remove step will go here</p>
-  </div>
-);
+class StepListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={};
+
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleDelete() {
+    this.props.removeStep(this.props.step.id);
+  }
+
+  render() {
+    const { step } = this.props;
+
+    return(
+      <div>
+        <h4>{step.title}</h4>
+        <p>{step.body}</p>
+
+        <p>Button to toggle step status will go here</p>
+
+        <button onClick={this.handleDelete}>Delete</button>
+      </div>
+    )
+  }
+}
 
 export default StepListItem;
