65594bb9245dfefe2bdb08cf2d66a8ffa51feef2
Renata Cummins <renatacummins@gmail.com>
2019-09-10 10:40:24 -0400

    feature: add thunk middleware

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,8 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
+import thunk from '../middleware/thunk';
 
-const configureStore = () => (
-  createStore(rootReducer)
+const configureStore = (preloadedState = {}) => (
+  createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(thunk))
 );
 
 export default configureStore;
