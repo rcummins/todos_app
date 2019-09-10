c9418285feb840c366baf47422cad78769cef504
Renata Cummins <renatacummins@gmail.com>
2019-09-10 11:00:11 -0400

    feature: add thunk action creator to fetch todos from database

--- frontend/actions/todo_actions.js
+++ frontend/actions/todo_actions.js
@@ -1,3 +1,5 @@
+import * as TodoAPIUtil from '../util/todo_api_util';
+
 export const RECEIVE_TODOS = "RECEIVE_TODOS";
 export const RECEIVE_TODO = "RECEIVE_TODO";
 export const REMOVE_TODO = "REMOVE_TODO";
@@ -16,3 +18,9 @@ export const removeTodo = id => ({
   type: REMOVE_TODO,
   id
 });
+
+export const fetchTodos = () => dispatch => (
+  TodoAPIUtil.fetchTodos().then(
+    todos => dispatch(receiveTodos(todos))
+  )
+);
