e0e9b216da04b7ebd91d376180a4635b45d76122
Renata Cummins <renatacummins@gmail.com>
2019-09-17 19:03:42 -0400

    fix: set user's session_token if not set yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,6 @@ class User < ApplicationRecord
   private
 
   def ensure_session_token
-    self.session_token || SecureRandom.urlsafe_base64(16)
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
   end
 end
