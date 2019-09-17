329aceab7253a61b66595bd026158985e089e684
Renata Cummins <renatacummins@gmail.com>
2019-09-17 18:40:20 -0400

    feature: add signup component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './home/home';
 import TodoListContainer from './todos/todo_list_container';
+import SignupContainer from './session/signup_container';
 
 const App = () => (
   <div>
     <Route exact path="/" component={Home} />
     <Route path="/todos" component={TodoListContainer} />
+    <Route path="/signup" component={SignupContainer} />
   </div>
 );
 
---
+++ frontend/components/session/signup.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInput(key) {
+    return e => this.setState({ [key]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.createNewUser({ user: this.state }).then(
+      () => this.props.history.push('/todos')
+    );
+  }
+
+  render() {
+    return(
+      <div className="form signup">
+        <h2>Sign up</h2>
+        <form>
+
+          <div className="form-input">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              onChange={this.handleInput('username')}
+              value={this.state.username}
+            />
+          </div>
+
+          <div className="form-input">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              onChange={this.handleInput('password')}
+              value={this.state.password}
+            />
+          </div>
+
+          <button
+            className="button-signup"
+            onClick={this.handleSubmit} >Sign up</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Signup;
