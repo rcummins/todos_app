83f3de1c7b12fb5d4bf8cbeddba4932b763e1925
Renata Cummins <renatacummins@gmail.com>
2019-09-17 12:18:51 -0400

    feature: add home component for / route and route to /todos

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Home from './home/home';
 import TodoListContainer from './todos/todo_list_container';
 
 const App = () => (
   <div>
-    <h1>Todos App</h1>
-    <TodoListContainer />
+    <Route exact path="/" component={Home} />
+    <Route path="/todos" component={TodoListContainer} />
   </div>
 );
 
---
+++ frontend/components/home/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => (
+  <div className="home">
+    <div className="home-description">
+      <h1>Todos App</h1>
+      <h3>Keep track of all your todos in one place!</h3>
+      <ul>
+        <li>Quickly create simple todos with a title and description</li>
+        <li>For your more complex tasks, add detailed steps within a todo</li>
+        <li>Mark todos done or delete them when you're finished</li>
+      </ul>
+    </div>
+  </div>
+)
