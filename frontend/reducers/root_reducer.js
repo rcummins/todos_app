9c6928b56a9fc2b00529fc13baf6b44518beb591
Renata Cummins <renatacummins@gmail.com>
2019-09-17 15:27:40 -0400

    feature: add session reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,12 +3,14 @@ import todosReducer from './todos_reducer';
 import stepsReducer from './steps_reducer';
 import todoErrorReducer from './todo_error_reducer';
 import stepErrorReducer from './step_error_reducer';
+import sessionReducer from './session_reducer';
 
 const rootReducer = combineReducers({
   todos: todosReducer,
   steps: stepsReducer,
   todoErrors: todoErrorReducer,
-  stepErrors: stepErrorReducer
+  stepErrors: stepErrorReducer,
+  session: sessionReducer
 });
 
 export default rootReducer;
