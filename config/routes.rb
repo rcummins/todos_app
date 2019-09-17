8666bbb89bdb9aab8b4cbffc55a8e909927399cb
Renata Cummins <renatacummins@gmail.com>
2019-09-17 14:52:04 -0400

    feature: add show route and view for users

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :todos, only: [:index, :create, :show, :update, :destroy]
     resources :steps, only: [:index, :create, :show, :update, :destroy]
-    resources :users, only: [:create]
+    resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy]
   end
 end
