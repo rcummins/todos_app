8047d8a40be453461bbd57fe1bec0e672dc20674
Renata Cummins <renatacummins@gmail.com>
2019-09-17 21:22:33 -0400

    feature: bootstrap current user by setting it on window

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -4,10 +4,18 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-const store = configureStore();
-window.store = store;
-
 document.addEventListener('DOMContentLoaded', () => {
+  let preloadedState;
+  if (window.currentUser) {
+    preloadedState = {
+      session: {
+        currentUser: window.currentUser
+      }
+    };
+  }
+
+  const store = configureStore(preloadedState);
+
   ReactDOM.render(
     <Root store={store} />,
     document.getElementById("content")
