5d56e69521921b896360771a733afbbda7d13795
Renata Cummins <renatacummins@gmail.com>
2019-09-18 13:55:07 -0400

    feature: add preview image to home component

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 export default () => (
   <div className="home">
     <div className="home-description">
-      <h1>Todos App</h1>
-      <h3>Keep track of all your todos in one place!</h3>
+      <h2>Your todos. Tracked. Organized.</h2>
       <ul>
         <li>Quickly create simple todos with a title and description</li>
-        <li>For your more complex tasks, add detailed steps within a todo</li>
+        <li>For more complex tasks, add detailed steps within a todo</li>
         <li>Mark todos done or delete them when you're finished</li>
       </ul>
     </div>
+
+    <img
+      className="home-preview"
+      src="https://renatacumminstodosapp.s3.amazonaws.com/todos_app_preview.png"
+      alt="app preview" />
   </div>
 )
