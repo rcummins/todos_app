cdb988f7445746ce68b8eb7f4f24e27cbbeab7e1
Renata Cummins <renatacummins@gmail.com>
2019-09-18 08:31:10 -0400

    feature: add login component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import Home from './home/home';
+import NavBar from './nav_bar/nav_bar_container';
 import TodoListContainer from './todos/todo_list_container';
 import SignupContainer from './session/signup_container';
-import NavBar from './nav_bar/nav_bar_container';
+import LoginContainer from './session/login_container';
 
 const App = () => (
   <div>
@@ -12,6 +13,7 @@ const App = () => (
     <Route exact path="/" component={Home} />
     <ProtectedRoute path="/todos" component={TodoListContainer} />
     <AuthRoute path="/signup" component={SignupContainer} />
+    <AuthRoute path="/login" component={LoginContainer} />
   </div>
 );
 
---
+++ frontend/components/session/login.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInput(key) {
+    return e => this.setState({ [key]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.login({ user: this.state }).then(
+      () => this.props.history.push('/todos')
+    );
+  }
+
+  render() {
+    return(
+      <div className="form login">
+        <h2>Log in</h2>
+        <form>
+
+          <div className="form-input">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              onChange={this.handleInput('username')}
+              value={this.state.username}
+            />
+          </div>
+
+          <div className="form-input">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              onChange={this.handleInput('password')}
+              value={this.state.password}
+            />
+          </div>
+
+          <button
+            className="button-login"
+            onClick={this.handleSubmit}>Log in</button>
+        </form>
+      </div>
+    )
+  }
+};
+
+export default Login;
