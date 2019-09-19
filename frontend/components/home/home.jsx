1e3fbe61cbbf1ab72ad97f7cd2559166cb61ca1c
Renata Cummins <renatacummins@gmail.com>
2019-09-19 09:38:09 -0400

    feature: change styles of home component to evoke hero image

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default () => (
-  <div className="home">
-    <div className="home-description">
-      <h2>Your todos. Tracked. Organized.</h2>
+  <div className="hero">
+    <div className="hero-text">
+      <p>Your todos.</p>
+      <p>Tracked.</p>
+      <p>Organized.</p>
       <ul>
         <li>Quickly create simple todos with a title and description</li>
         <li>For more complex tasks, add detailed steps within a todo</li>
@@ -12,7 +14,7 @@ export default () => (
     </div>
 
     <img
-      className="home-preview"
+      className="hero-image"
       src="https://renatacumminstodosapp.s3.amazonaws.com/todos_app_preview.png"
       alt="app preview" />
   </div>
